bd38fdf59978130c52deeb27437feb191d4d1748
long2ice <343178315@qq.com>
2018-05-25 21:43:38 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # crontab-ss
-定时向https://my.ishadowx.net/抓取免费ss账号并生成配置文件
+定时向https://my.ishadowx.net/ 抓取免费ss账号并生成配置文件
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-定时向https://my.ishadowx.net/抓取免费ss账号并生成配置文件
+定时向https://my.ishadowx.net/ 抓取免费ss账号并生成配置文件
 """
 import json
 import os
